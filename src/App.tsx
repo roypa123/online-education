685073d7488e2ae34d800e695ff0e8624299724b
Roy P A <roy@nintrivalabs.com>
2025-07-22 14:57:47 +0530

    fix: some changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Header from "./shared/header/Header";
 function App() {
   return (
     <div className="w-full h-full">
-        <div className="h-[1078px] flex flex-col bg-[#FDF8EE]">
+        <div className="h-[800px] flex flex-col bg-[#FDF8EE]">
                 <div className="h-1/14">
                       <Header  />
                 </div>
@@ -16,18 +16,22 @@ function App() {
                <div className="h-12/14 ml-8 mr-8 mt-8 mb-8"
                 style={{ backgroundImage: `url(${FirstBackground})` }}
                >
-                  <div className="flex ml-45 mr-45 mt-16 mb-8">
-                     <div className="w-4/8">
-                        <div>
-                            The Smart
+                  <div className="flex ml-45 mr-45 mt-8 mb-8">
+                     <div className="w-4/8 mt-30 space-y-4" >
+                        <div className="text-5xl font-bold">
+                            <span>The </span>
+                            <span className="text-[#FF7426]">Smart</span> 
                         </div>
-                         <div>
-                            Choice For Future
+                         <div className="text-5xl font-bold">
+                           <span>Choice For </span>
+                            <span className="text-[#FF7426]">Future</span>
+                            
                         </div>
-                        <div>
+                        <div className="text-1xl font-medium text-[#8A8A8A]">
                           Elearn is a global training provider based acress the UK that specialises in accredited and bespoke training courses. We crush the ...
                         </div>
-                     
+                     </div>
+                     <div className="w-1/8 ">
                      </div>
                      <div className="w-5/8 ">
                      <img src={FirstImage} alt=""/>
