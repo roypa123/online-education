22d99fb97cb00029e4a6c3dae90bc113d56a07df
Roy P A <roy@nintrivalabs.com>
2025-07-22 12:23:28 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+
+import FirstBackground from "./assets/first_background.svg";
+import FirstImage from "./assets/first_image.svg"
+import Header from "./shared/header/Header";
+
+
+function App() {
+  return (
+    <div className="w-full h-full">
+        <div className="h-[1078px] flex flex-col bg-[#FDF8EE]">
+                <div className="h-1/14">
+                      <Header  />
+                </div>
+                <div className="h-1/14">
+                </div>
+               <div className="h-12/14 ml-8 mr-8 mt-8 mb-8"
+                style={{ backgroundImage: `url(${FirstBackground})` }}
+               >
+                  <div className="flex ml-45 mr-45 mt-16 mb-8">
+                     <div className="w-4/8">Left Box</div>
+                     <div className="w-5/8 ">
+                     <img src={FirstImage} alt=""/>
+                     </div>
+                  </div>
+               </div>
+               
+        </div>
+        <div>
+          fvffvfvfv
+        </div>
+    </div>
+  )
+}
+
+export default App
+
+
+
+    {/* <img src={FirstBackground} alt="" className=" h-[900px]" /> */}
---
+++ src/shared/header/Header.tsx
@@ -0,0 +1,12 @@
+
+function Header() {
+  
+
+  return (
+    <div className="h-[100px] bg-blue-500">
+      <h2>Header</h2>
+    </div>
+  )
+}
+
+export default Header
