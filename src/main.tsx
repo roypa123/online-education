ae8b1edf52d2064de6ece699ca59c5ed505e0e4e
Roy P A <roy@nintrivalabs.com>
2025-07-23 11:04:46 +0530

    fix: some changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './styles/fonts.css'; 
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
--- src/shared/header/Header.tsx
+++ src/shared/header/Header.tsx
@@ -1,10 +1,28 @@
+import { Link } from "react-router-dom"
+import logo from '../../assets/logo.svg';
 
 function Header() {
   
 
   return (
-    <div className="h-[100px] bg-blue-500">
-      <h2>Header</h2>
+    <div className="h-[100px] flex px-10  justify-between items-center ">
+        <div className="flex items-center space-x-2">
+          <img src={logo} alt="" className="w-10 h-8" />
+          <div className="text-[20px] font-bold">Book Store</div>
+        </div>
+        <div className="flex space-x-4 items-center">
+          <nav className="flex space-x-6">
+            <Link to="" className="text-[16px] font-semibold">Home</Link>
+            <Link to="" className="text-[16px] font-semibold">About us</Link>
+            <Link to="" className="text-[16px] font-semibold">Courses</Link>
+            <Link to="" className="text-[16px] font-semibold">Our Service</Link>
+            <Link to="" className="text-[16px] font-semibold">Contact us</Link>
+          </nav>
+          <div>
+             <button className="bg-[#4D2C5E] font-bold text-white px-8 py-2 rounded-full ml-4">Sign in</button>
+          </div>
+        </div>
+        
     </div>
   )
 }
